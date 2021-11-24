e7d98071e2e395852cb150abc6fa3154a98ccc51
Martin Šepeľa <xsepela@fi.muni.cz>
2021-11-24 20:04:34 +0100

    Added: - state keeper for Temporary data => data that is saved only till next request or till they are read without being saved - keys for temp data so that it is easier to work with them - added sessions - added state for reservation (getting and setting it)

---
+++ TeamProject (Book Reservation)/MVCProject/StateManager/StateKeeper.cs
@@ -0,0 +1,128 @@
+﻿using BL.DTOs.Entities.Reservation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.StateManager
+{
+    public sealed class StateKeeper
+    {
+        // Singleton pattern 
+
+        private static readonly StateKeeper instance = new StateKeeper();
+        static StateKeeper()
+        {
+        }
+        private StateKeeper()
+        {
+        }
+        public static StateKeeper Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        // Singleton pattern        
+
+        // Temporary data
+
+        /// <summary>
+        /// Saves the object into temporary data till next request.
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void AddTillNextRequest(Controller controller, TempDataKeys key, object value)
+        {
+            controller.TempData[key.ToString()] = value;
+        }
+
+        /// <summary>
+        /// Reads and destroys the value unless saving was called before
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public object ReadAndDestroy(Controller controller, TempDataKeys key)
+        {
+            return controller.TempData[key.ToString()];
+        }
+
+        public object ReadAndDestroy(ITempDataDictionary tempData, TempDataKeys key)
+        {
+            return tempData[key.ToString()];
+        }
+
+        /// <summary>
+        /// Peeks at the value and doesnt destroy it immidiately even if the save wasnt called
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public object ReadAndDestroyForNextRequest(Controller controller, TempDataKeys key)
+        {
+            return controller.TempData.Peek(key.ToString());
+        }
+
+        public object ReadAndDestroyForNextRequest(ITempDataDictionary tempData, TempDataKeys key)
+        {
+            return tempData.Peek(key.ToString());
+        }
+
+        /// <summary>
+        /// Saves all values in temporary data so even when ReadAndDestroyNow is called they wont be destroyed
+        /// </summary>
+        /// <param name="controller"></param>
+        public void SaveExistingObjectsForNextRequest(Controller controller)
+        {
+            controller.TempData.Keep();
+        }
+
+        /// <summary>
+        /// Saves a specific object in temporary data so even when ReadAndDestroyNow is called they wont be destroyed
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="key"></param>
+        public void SaveSpecificObjectForNextRequest(Controller controller, TempDataKeys key)
+        {
+            controller.TempData.Keep(key.ToString());
+        }
+
+        // Temporary data
+
+        // Session data
+
+        private const string _reservationKey = "currentReservation";
+
+        /// <summary>
+        /// Saves this reservation into sessions for at least 5 minutes
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="newReserv"></param>
+        public void SetReservationInSession(Controller controller, ReservationDTO newReserv)
+        {
+            controller.HttpContext.Session.Set<ReservationDTO>(_reservationKey, newReserv);
+        }
+
+        /// <summary>
+        /// Gets the current active reservation in current session
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <returns></returns>
+        public ReservationDTO GetReservationInSession(Controller controller)
+        {
+            return controller.HttpContext.Session.Get<ReservationDTO>(_reservationKey);
+        }
+
+        public ReservationDTO GetReservationInSession(ISession currSession)
+        {
+            return currSession.Get<ReservationDTO>(_reservationKey);
+        }
+    }
+}
