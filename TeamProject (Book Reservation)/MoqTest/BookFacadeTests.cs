35e614a3436909890b39273b0a0e07091a6b2ebb
Martin Šepeľa <xsepela@fi.muni.cz>
2021-12-09 15:49:38 +0100

    Added: - tests for BookFacade

--- TeamProject (Book Reservation)/MoqTest/BookFacadeTests.cs
+++ TeamProject (Book Reservation)/MoqTest/BookFacadeTests.cs
@@ -65,17 +65,78 @@ namespace MoqTest
         }
 
         [Fact]
-        public async Task GetBookPreviewsComplex()
+        public async Task GetBook1()
         {
             using (var mock = AutoMock.GetLoose())
             {
-                var bookFacade = Setup(mock,GetEntries);
+                var bookFacade = Setup(mock, GetEntries);
 
                 var data = GetEntries();
-
-                var result = await bookFacade.Get(data.Item1.Id, data.Item2, data.Item3);
-                
+                await Evaluate(mock, bookFacade, data);
             }
+        }
+
+        [Fact]
+        public async Task GetBook2()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var bookFacade = Setup(mock, GetEntries2);
+
+                var data = GetEntries2();
+                await Evaluate(mock, bookFacade, data);
+            }
+        }
+
+        [Fact]
+        public async Task GetBook3()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var bookFacade = Setup(mock, GetEntries3);
+
+                var data = GetEntries3();
+                await Evaluate(mock, bookFacade, data);
+            }
+        }
+
+        private static async Task Evaluate(AutoMock mock, BookFacade bookFacade, Tuple<Book, string[], string[]> data)
+        {
+            var result = await bookFacade.Get(data.Item1.Id, data.Item2, data.Item3);
+
+            Assert.True(mock.Mock<IRepository<Book>>().Invocations.Count() == 1);
+
+            foreach (var review in data.Item1.Reviews)
+            {
+                UserRepoInvocationsInclude(mock, review.UserID);
+            }
+
+            foreach (var bookInstance in data.Item1.BookInstances)
+            {
+                UserRepoInvocationsInclude(mock, bookInstance.BookOwnerId);
+                ReservBookInstInvocationsInclude(mock, bookInstance.Id);
+            }
+
+            Assert.True(mock.Mock<IQuery<ReservationBookInstance>>()
+                .Invocations.Where(invo => invo.Method.Name == nameof(IQuery<ReservationBookInstance>.Execute)).Count() == data.Item1.BookInstances.Count());
+        }
+
+        private static void UserRepoInvocationsInclude(AutoMock mock, object argument)
+        {
+            var predicates = mock.Mock<IRepository<User>>().Invocations
+                                .Where(invo => invo.Method.Name == nameof(IRepository<User>.GetByID))
+                                .Select(invo => (int) invo.Arguments[0]);
+
+            Assert.Contains(predicates, pred => pred.Equals(argument));
+        }
+
+        private static void ReservBookInstInvocationsInclude(AutoMock mock, object argument)
+        {
+            var predicates = mock.Mock<IQuery<ReservationBookInstance>>().Invocations
+                                .Where(invo => invo.Method.Name == nameof(IQuery<ReservationBookInstance>.Where))
+                                .Select(invo => (SimplePredicate)invo.Arguments[0]).ToList();
+
+            Assert.Contains(predicates, pred => pred.ComparedValue.Equals(argument));
         }
 
         public Tuple<Book,string[],string[]> GetEntries()
@@ -117,6 +178,74 @@ namespace MoqTest
             {
                 nameof(BookDTO.Reviews),
                 nameof(BookDTO.BookInstances)
+            };
+
+            var refsToLoad = new string[0];
+
+            return new Tuple<Book, string[], string[]>(book, refsToLoad, colToLoad);
+        }
+
+        public Tuple<Book, string[], string[]> GetEntries2()
+        {
+            var bookInst1 = new BookInstance()
+            {
+                Id = 1,
+                BookOwnerId = 1
+            };
+
+            var review1 = new Review()
+            {
+                UserID = 1
+            };
+
+            var book = new Book()
+            {
+                Id = 1,
+                BookInstances = new List<BookInstance>()
+                {
+                    bookInst1
+                },
+                Reviews = new List<Review>
+                {
+                    review1
+                }
+            };
+
+            var colToLoad = new string[]
+            {
+                nameof(BookDTO.Reviews),
+                nameof(BookDTO.BookInstances)
+            };
+
+            var refsToLoad = new string[0];
+
+            return new Tuple<Book, string[], string[]>(book, refsToLoad, colToLoad);
+        }
+
+        public Tuple<Book, string[], string[]> GetEntries3()
+        {
+            var review1 = new Review()
+            {
+                UserID = 1
+            };
+            var review2 = new Review()
+            {
+                UserID = 2
+            };
+
+            var book = new Book()
+            {
+                Id = 1,                
+                Reviews = new List<Review>
+                {
+                    review1, review2
+                },
+                BookInstances = new List<BookInstance>()                
+            };
+
+            var colToLoad = new string[]
+            {
+                nameof(BookDTO.Reviews),
             };
 
             var refsToLoad = new string[0];
